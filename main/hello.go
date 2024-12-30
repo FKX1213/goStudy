934770ea779b27557a848d46357be7dff17a3e62
FKX1213 <652317399@qq.com>
2024-12-30 09:34:11 +0800

    feat: var 变量学习，多个变量

--- main/hello.go
+++ main/hello.go
@@ -3,23 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	// 方式一：声明一个变量，默认的值是0
-	var a int
-	fmt.Println("a = ", a)
-	fmt.Printf("tyoe of a = %T\n", a)
+	// 声明多个变量
+	var xx, yy int = 100, 200
+	var s1, s2 string = "hello", "world"
+	fmt.Println("xx= ", xx, ", yy= ", yy, ", s1= ", s1, ", s2= ", s2)
 
-	// 方式二：声明一个变量，初始化一个值
-	var b int = 100
-	fmt.Println("b = ", b)
-	fmt.Printf("tyoe of b = %T\n", b)
-
-	// 方法三：初始化的时候, 省去数据类型，通过值自动匹配当前的变量的数据类型
-	var c = 100
-	fmt.Println("c = ", c)
-	fmt.Printf("tyoe of c = %T\n", c)
-
-	// 方法四：(常用) 省去var关键字，直接自动匹配
-	d := 100
-	fmt.Println("d = ", d)
-	fmt.Printf("tyoe of d = %T\n", d)
+	// 多变量声明，多行
+	var (
+		cc int     = 100
+		dd float32 = 3.14
+		ee bool    = true
+	)
+	fmt.Println("cc= ", cc, ", dd= ", dd, ", ee= ", ee)
 }
