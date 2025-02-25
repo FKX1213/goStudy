3a3420b5660bcbcffedc5aa759226cfafb9d8123
FKX1213 <652317399@qq.com>
2025-02-25 17:56:52 +0800

    feat: 切片

--- lessonCodes/lesson04.go
+++ lessonCodes/lesson04.go
@@ -1,20 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 /**
  * @date:       2025/2/25 16:21
  * @author:     cola1213
- * @description:数组
+ * @description:数组、切片
  */
 func main() {
-	// 数组在声明是长度只能是一个常量，不能是变量
-	var array = [5]int{1, 2, 4, 3, 5}
-	fmt.Println(array)
+	// 切片
+	var nums []int
+	nums = append(nums, 1)
+	nums = append(nums, 3)
 
-	// 数组的使用
-	fmt.Println(array[0])
-	array[0] = 0
-	fmt.Println(array[0])
-	fmt.Println(len(array))
+	fmt.Println(nums[0])
+	fmt.Println(len(nums))
+	fmt.Println(cap(nums))
+
+	// 定义一个字符串切片
+	var list []string
+	fmt.Println(list == nil) // true
+
+	// 定义一个字符串切片
+	var list1 = make([]string, 0)
+	fmt.Println(list1, len(list1), cap(list1))
+	fmt.Println(list1 == nil) // false
+
+	list2 := make([]int, 2, 2)
+	fmt.Println(list2, len(list2), cap(list2))
+
+	// 切片是数组切出来的
+	var nums01 = [3]int{1, 2, 3}
+	nums02 := nums01[0:2] // 1,2
+	fmt.Println(nums02)
+
+	// 切片排序
+	nums03 := []int{1, 2, 7, 3, 8, 6, 9}
+	sort.Ints(nums03) // 默认是从小到大
+	fmt.Println(nums03)
+	sort.Sort(sort.Reverse(sort.IntSlice(nums03))) // 反转，从大到小
+	fmt.Println(nums03)
 }
